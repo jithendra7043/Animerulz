a9c4742aa3270e964eacdb2150657040742ec2b2
jithendra7043 <144207863+jithendra7043@users.noreply.github.com>
2023-09-26 13:30:50 +0530

    Add files via upload

---
+++ scripts/redirectFromMainPage.js
@@ -0,0 +1,6 @@
+const url = window.location.href;
+const urlList = url.split("/");
+
+if(urlList[3] == "myheroacademiaseason2"){
+    window.location.href = "https://animerulz.in/myheroacademia2";
+}
