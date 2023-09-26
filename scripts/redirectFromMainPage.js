e0633aafbfe91e552c96069450288dc04b5f061b
jithendra7043 <144207863+jithendra7043@users.noreply.github.com>
2023-09-26 17:45:21 +0530

    Update redirectFromMainPage.js

--- scripts/redirectFromMainPage.js
+++ scripts/redirectFromMainPage.js
@@ -1,6 +1,27 @@
-const url = window.location.href;
+// const url = window.location.href;
+url = "https://animerulz.in/myheroacademiaseason2/"
 const urlList = url.split("/");
+let index;
 
-if(urlList[3] == "myheroacademiaseason2"){
-    window.location.href = "https://animerulz.in/myheroacademia2";
+const redirectThis = [
+    "myheroacademiaseason2",
+    "Naruto",
+    "MyHeroAcademia",
+    "DragonBallSuper"
+]
+const redirectTo = [
+    "myheroacademia2",
+    "naruto",
+    "myheroacademia",
+    "dragonballsuper"
+]
+
+for(let i = 1; i <= redirectThis.length ; i ++){
+    if(urlList[3] == redirectThis[i - 1]){
+        index = i;
+        break;
+    }
 }
+
+if(index)
+    window.location.href = "https://animerulz.in/" + redirectTo[index - 1];
